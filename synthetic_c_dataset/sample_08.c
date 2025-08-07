71679a30a4f493841de67052ba246c3609b1ca40
bruno-trindade85 <brunocmt@gmail.com>
2025-08-07 08:18:27 -0300

    Update sample_08.c

--- synthetic_c_dataset/sample_08.c
+++ synthetic_c_dataset/sample_08.c
@@ -1,13 +1,22 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void exemplo() {
-    char *buffer = (char *)malloc(100);
-    if (buffer == NULL) return;
-    printf("Usando o buffer...\n");
+char* global;
+
+void registrar() {
+    char buffer[100];
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
+
+    global = malloc(strlen(buffer) + 1);
+    strcpy(global, buffer);
 }
 
 int main() {
-    exemplo();
+    registrar();
+    printf("Registrado: %s\n", global);
+    
     return 0;
 }
+
