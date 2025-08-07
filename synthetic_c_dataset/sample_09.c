9170936c65e530c3367b144875b7bc642c3dadd0
bruno-trindade85 <brunocmt@gmail.com>
2025-08-07 08:19:07 -0300

    Update sample_09.c

--- synthetic_c_dataset/sample_09.c
+++ synthetic_c_dataset/sample_09.c
@@ -1,13 +1,22 @@
-#include <stdlib.h>
 #include <stdio.h>
-
-void exemplo() {
-    char *buffer = (char *)malloc(100);
-    if (buffer == NULL) return;
-    printf("Usando o buffer...\n");
-}
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    exemplo();
+    char* blocos[5];
+    char buffer[50];
+
+    for (int i = 0; i < 5; i++) {
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        blocos[i] = malloc(strlen(buffer) + 1);
+        strcpy(blocos[i], buffer);
+
+        if (strcmp(buffer, "parar") == 0)
+            break; 
+    }
+
     return 0;
 }
+
